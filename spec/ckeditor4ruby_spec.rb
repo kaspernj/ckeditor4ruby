be81121d85d9819ff8a91ed549862af894043f1a
Kasper Johansen <k@spernj.org>
2012-03-04 12:35:44 +0100

    Initial commit to ckeditor4ruby.

---
+++ spec/ckeditor4ruby_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Ckeditor4ruby" do
+  it "fails" do
+    fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
